6e5e8b14d240284e1a9b9a10baa04e97c579e0ea
perkzen <perko.domen@gmail.com>
2022-02-28 14:14:21 +0100

    Initial Commit

---
+++ expense-tracker/expense_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  expense_trackerApp.swift
+//  expense-tracker
+//
+//  Created by Domen Perko on 28/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct expense_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
